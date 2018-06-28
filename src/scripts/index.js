8295efc54ea67ebc9b8aa16c22621f042abaa7d8
Ash Kyd <ash@kyd.com.au>
2018-06-28 16:51:04 +1000

    Update uglify + new site (#4)
    * update to uglify harmony for es6+ support
    * remove Chrome app support
    * add PWA manifest
    * new website makes launching the app easier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,322 +1,310 @@
-try{
-  navigator.serviceWorker.register('workerService.js');
-}catch(e){
+try {
+  navigator.serviceWorker.register("workerService.js");
+} catch (e) {
   console.error(e);
 }
 
 // Globals and general ugliness.
-window.$ = require('jquery');
+window.$ = require("jquery");
 window.jQuery = $;
-require('../../node_modules/bootstrap/dist/js/bootstrap.min.js');
-var versioning = require('./_includes/versioning');
-// Ace editor
-var ace = require('brace');
-require('brace/mode/text');
-require('brace/mode/javascript');
-require('brace/mode/xml');
-require('brace/mode/css');
-require('brace/mode/json');
-require('brace/mode/html');
-require('brace/ext/searchbox');
-require('brace/theme/chrome');
+require("../../node_modules/bootstrap/dist/js/bootstrap.min.js");
+var versioning = require("./_includes/versioning");
 
 var editor;
 var before;
 
 // Formats we support.
 var formats = {
-    Text: {
-        name: 'Text',
-        filename: /\.txt$/,
-        parser: 'text',
-        mode: 'ace/mode/text',
-        actions: []
-    },
-    JavaScript: {
-        name: 'JavaScript',
-        filename: /\.js$/,
-        parser: 'js',
-        mode: 'ace/mode/javascript',
-        actions: ['compress','prettify']
-    },
-    HTML: {
-        name: 'HTML',
-        filename: /\.html?$/,
-        parser: 'html',
-        mode: 'ace/mode/html',
-        actions: ['compress','prettify']
-    },
-    CSS: {
-        name: 'CSS',
-        filename: /\.js$/,
-        parser: 'css',
-        mode: 'ace/mode/css',
-        actions: ['compress','prettify']
-    },
-    XML: {
-        name: 'XML',
-        filename: /\.xml$/,
-        parser: 'xml',
-        mode: 'ace/mode/xml',
-        actions: ['compress','prettify']
-    },
-    JSON: {
-        name: 'JSON',
-        filename: /\.json$/,
-        parser: 'json',
-        mode: 'ace/mode/json',
-        actions: ['compress','prettify']
-    },
+  Text: {
+    name: "Text",
+    filename: /\.txt$/,
+    parser: "text",
+    mode: "ace/mode/text",
+    actions: []
+  },
+  JavaScript: {
+    name: "JavaScript",
+    filename: /\.js$/,
+    parser: "js",
+    mode: "ace/mode/javascript",
+    actions: ["compress", "prettify"]
+  },
+  HTML: {
+    name: "HTML",
+    filename: /\.html?$/,
+    parser: "html",
+    mode: "ace/mode/html",
+    actions: ["compress", "prettify"]
+  },
+  CSS: {
+    name: "CSS",
+    filename: /\.js$/,
+    parser: "css",
+    mode: "ace/mode/css",
+    actions: ["compress", "prettify"]
+  },
+  XML: {
+    name: "XML",
+    filename: /\.xml$/,
+    parser: "xml",
+    mode: "ace/mode/xml",
+    actions: ["compress", "prettify"]
+  },
+  JSON: {
+    name: "JSON",
+    filename: /\.json$/,
+    parser: "json",
+    mode: "ace/mode/json",
+    actions: ["compress", "prettify"]
+  }
 };
 
-function formatChange(format){
-    changeFormat($('.formats').val());
+function formatChange(format) {
+  changeFormat($(".formats").val());
 }
 
-function changeFormat(format){
-    $('.formats').val(format);
-    var $actions = $('.action').attr('disabled','disabled');
-    formats[format].actions.forEach(function(action){
-        $actions.filter('.'+action).removeAttr('disabled');
-    });
-    editor.getSession().setMode(formats[format].mode);
+function changeFormat(format) {
+  $(".formats").val(format);
+  var $actions = $(".action").attr("disabled", "disabled");
+  formats[format].actions.forEach(function(action) {
+    $actions.filter("." + action).removeAttr("disabled");
+  });
+  editor.getSession().setMode(formats[format].mode);
 }
 
-function performAction(){
-    var format = $('.formats').val();
-    var action = $(this).data('action');
-    var input = editor.getValue();
-    before = input.length;
-    postMessage({
-        lang: formats[format].parser,
-        direction: action,
-        input: input,
-    });
+function performAction() {
+  var format = $(".formats").val();
+  var action = $(this).data("action");
+  var input = editor.getValue();
+  before = input.length;
+  postMessage({
+    lang: formats[format].parser,
+    direction: action,
+    input: input
+  });
+  return false;
+}
+
+function initDrag() {
+  var holder = document.getElementsByTagName("body")[0];
+
+  holder.ondragover = function() {
+    this.className = "hover";
     return false;
-}
-
-function initDrag(){
-    var holder = document.getElementsByTagName('body')[0];
-
-    holder.ondragover = function () { this.className = 'hover'; return false; };
-    holder.ondragend = function () { this.className = ''; return false; };
-    holder.ondrop = function (e) {
-        this.className = '';
-        e.preventDefault();
-
-        dragSingle(e.dataTransfer.files[0]);
-        return false;
-    };
-}
-
-function dragSingle(file){
-    var reader = new FileReader();
-
-    reader.onload = function (event) {
-        // Uglify occasionally inserts \x01 for some reason.
-        var isBinary = /[\x00\x02-\x08\x0E-\x1F]/.test(event.target.result);
-        if(isBinary){
-            window.bin = event.target.result;
-            message('Binary file detected. Loading as base64.');
-            reader.readAsDataURL(file);
-        } else {
-            editor.setValue('');
-            detectContentType(file.name,event.target.result);
-            editor.setValue(event.target.result);
-        }
-    };
-
-    reader.readAsText(file);
-}
-
-
-
-function detectContentType(filename, content){
-    var format;
-    format = detectContentTypeFromExtension(filename);
-    if(!format){
-        format = detectContentTypeFromContent(content);
-    }
-    if(!format){
-        format = 'Text';
-    }
-    changeFormat(format);
-}
-function detectContentTypeFromExtension(filename){
-    for(var i in formats){
-        if(filename.match(formats[i].filename)){
-            return i;
-        }
-    }
-}
-function detectContentTypeFromContent(content){
-    var round1 = [
-        // Matches an initial /* comment */ and subsequent function def.
-        {
-            test : /^\/\*(.|\n)+function\s*\(/,
-            type : 'JavaScript'
-        },
-        {
-            test : /^\s*</,
-            type : 'XML'
-        },
-        {
-            test : /<html(.*?)?>/,
-            type : 'HTML'
-        },
-        {
-            test : /^\s*{/,
-            type : 'JSON'
-        },
-        {
-            test : /function\s*\(/,
-            type : 'JavaScript'
-        },
-        {
-            test : /@import/,
-            type : 'CSS'
-        },
-        // Matches something like "foo {", a syntax you'd likely
-        // not find elsewhere
-        // False positive on var a=\n//foo\n{a:b}
-        {
-            test : /\w+\s*\{/,
-            type : 'CSS'
-
-        },
-        // Matches assignment, excluding something like the CSS syntax [foo=bar]
-        // False positive on something like @import("/foo?a=b")
-        {
-            test : /\s[^'"\[]+\=/,
-            type : 'JavaScript'
-        }
-
-    ];
-
-    for(var i=0; i<round1.length; i++){
-        if(content.match(round1[i].test)){
-            return round1[i].type;
-        }
-    }
-
+  };
+  holder.ondragend = function() {
+    this.className = "";
     return false;
+  };
+  holder.ondrop = function(e) {
+    this.className = "";
+    e.preventDefault();
 
+    dragSingle(e.dataTransfer.files[0]);
+    return false;
+  };
 }
 
-function bytesToDisplay(bytes,units){
-    return Math.round((bytes)/10.24)/100 + (units ? ' KB' : '');
+function dragSingle(file) {
+  var reader = new FileReader();
+
+  reader.onload = function(event) {
+    // Uglify occasionally inserts \x01 for some reason.
+    var isBinary = /[\x00\x02-\x08\x0E-\x1F]/.test(event.target.result);
+    if (isBinary) {
+      window.bin = event.target.result;
+      message("Binary file detected. Loading as base64.");
+      reader.readAsDataURL(file);
+    } else {
+      editor.setValue("");
+      detectContentType(file.name, event.target.result);
+      editor.setValue(event.target.result);
+    }
+  };
+
+  reader.readAsText(file);
 }
 
-function message(msg){
-    $('.status.message').text(msg);
+function detectContentType(filename, content) {
+  var format;
+  format = detectContentTypeFromExtension(filename);
+  if (!format) {
+    format = detectContentTypeFromContent(content);
+  }
+  if (!format) {
+    format = "Text";
+  }
+  changeFormat(format);
+}
+function detectContentTypeFromExtension(filename) {
+  for (var i in formats) {
+    if (filename.match(formats[i].filename)) {
+      return i;
+    }
+  }
+}
+function detectContentTypeFromContent(content) {
+  var round1 = [
+    // Matches an initial /* comment */ and subsequent function def.
+    {
+      test: /^\/\*(.|\n)+function\s*\(/,
+      type: "JavaScript"
+    },
+    {
+      test: /<html(.*)?>/,
+      type: "HTML"
+    },
+    {
+      test: /<(div|span|ul|li|head|script)(.*)?>/,
+      type: "HTML"
+    },
+    {
+      // Matches a partial arrow function
+      test: /\)\s?=>/,
+      type: "JavaScript"
+    },
+    {
+      test: /^\s*{/,
+      type: "JSON"
+    },
+    {
+      test: /console\.log/,
+      type: "JavaScript"
+    },
+    {
+      test: /function\s*\(/,
+      type: "JavaScript"
+    },
+    {
+      test: /@import/,
+      type: "CSS"
+    },
+    // Matches something like "foo {", a syntax you'd likely
+    // not find elsewhere
+    // False positive on var a=\n//foo\n{a:b}
+    {
+      test: /\w+\s*\{/,
+      type: "CSS"
+    },
+    // Matches assignment, excluding something like the CSS syntax [foo=bar]
+    // False positive on something like @import("/foo?a=b")
+    {
+      test: /\s[^'"\[]+\=/,
+      type: "JavaScript"
+    },
+    {
+      test: /^\s*</,
+      type: "XML"
+    }
+  ];
+
+  for (var i = 0; i < round1.length; i++) {
+    if (content.match(round1[i].test)) {
+      return round1[i].type;
+    }
+  }
+
+  return false;
 }
 
+function bytesToDisplay(bytes, units) {
+  return Math.round(bytes / 10.24) / 100 + (units ? " KB" : "");
+}
 
-var $frame;
-function initFrame(){
-    $frame = $('<iframe></iframe>')
-        .attr({
-             id: "if",
-             src: "sandbox.html",
-             style: "display:none",
-             'child-src': 'chrome-extension://kmkncknehdjjpkcoadfhplbhngpeinek/worker.js'
-        });
-    $('body').append($frame);
-    window.addEventListener('message', onmessage);
+function message(msg) {
+  $(".status.message").text(msg);
 }
 
 var worker;
-function initWorker(){
-    // If we don't have workers for whatever reason, fire up a frame.
-    if(typeof Worker !== 'function'){
-        return initFrame();
-    }
-    worker = new Worker('worker.js');
-    worker.onmessage = onmessage;
+function initWorker() {
+  // If we don't have workers for whatever reason, fire up a frame.
+  if (typeof Worker !== "function") {
+    return message("this browser doesn't support workers. I can't go on!");
+  }
+  worker = new Worker("worker.js");
+  worker.onmessage = onmessage;
 }
 
+function postMessage(payload) {
+  // If we've got a worker, use that! these should be mutually exclusive.
+  if (worker) {
+    worker.postMessage(payload);
+  }
 
-function postMessage(payload){
-    // If our frame is initialised we're probably using a Chrome extension
-    if($frame){
-        $frame[0].contentWindow.postMessage(payload, '*');
-    }
-
-    // If we've got a worker, use that! these should be mutually exclusive.
-    if(worker){
-        worker.postMessage(payload);
-    }
-
-    $('.progress').fadeIn(25);
-    $('button, select').attr('disabled', 'disabled');
-    message(payload.direction+'ing…');
+  $(".progress").fadeIn(25);
+  $("button, select").attr("disabled", "disabled");
+  message(payload.direction + "ing…");
 }
 
 function onmessage(event) {
-    $('.progress').fadeOut(25);
-    $('button, select').removeAttr('disabled');
-    var output = event.data.response.output;
-    var error = event.data.response.error;
-    editor.setValue(output);
-    if(error){
-        message(error);
-    } else {
-        message('New size: '+bytesToDisplay(output.length,true)+
-            ', saving '+bytesToDisplay(before - output.length,true));
-    }
+  $(".progress").fadeOut(25);
+  $("button, select").removeAttr("disabled");
+  var output = event.data.response.output;
+  var error = event.data.response.error;
+  editor.setValue(output);
+  if (error) {
+    message(error);
+  } else {
+    message(
+      "New size: " +
+        bytesToDisplay(output.length, true) +
+        ", saving " +
+        bytesToDisplay(before - output.length, true)
+    );
+  }
 }
 
-$(document).ready(function(){
-    // Chrome can't doooo workers in a sandbox.
-    // Or at least I can't work out how.
-    // https://plus.google.com/+AshKyd/posts/MhRH3BrE4NR
-    if(window.location.protocol === "chrome-extension:"){
-        initFrame();
-    } else {
-        initWorker();
-    }
-    $(window).resize();
-    editor = ace.edit('editor');
-    editor.setTheme('ace/theme/chrome');
-    var session = editor.getSession();
-    session.setMode('ace/mode/text');
-    session.setUseWrapMode(true);
-    session.setWrapLimitRange(null, null);
-    editor.renderer.setPrintMarginColumn(80);
-    editor.focus();
+$(document).ready(function() {
+  initWorker();
+  $(window).resize();
+  editor = ace.edit("editor");
+  editor.setTheme("ace/theme/chrome");
+  var session = editor.getSession();
+  session.setMode("ace/mode/text");
+  session.setUseWrapMode(true);
+  session.setWrapLimitRange(null, null);
+  editor.renderer.setPrintMarginColumn(80);
+  editor.focus();
 
-    editor.on('paste',function(content){
-        // Break out so the editor can actually have the new value set.
-        setTimeout(function(){
-            detectContentType('',editor.getValue());
-        });
+  editor.on("paste", function(content) {
+    // Break out so the editor can actually have the new value set.
+    setTimeout(function() {
+      detectContentType("", editor.getValue());
     });
+  });
 
-    editor.on('change',function(content){
-        $('.status.format').text($('.formats').val());
-        message(bytesToDisplay(editor.getValue().length,true));
-    });
+  editor.on("change", function(content) {
+    $(".status.format").text($(".formats").val());
+    message(bytesToDisplay(editor.getValue().length, true));
+  });
 
-    for(var i in formats){
-        $('.formats').append('<option>'+formats[i].name+'</option>')
-    };
-    $('.formats').change(formatChange);
-    changeFormat('Text');
+  for (var i in formats) {
+    $(".formats").append("<option>" + formats[i].name + "</option>");
+  }
+  $(".formats").change(formatChange);
+  changeFormat("Text");
 
-    $('button.action').click(performAction);
-    initDrag();
+  $("button.action").click(performAction);
+  initDrag();
 
-    versioning();
+  versioning();
 
+  message("Drag a file or paste from the clipboard");
+
+  document.body.classList.remove("preload");
+  document.body.classList.add("loaded");
 });
 
-$(window).resize(function(){
-    var bodyHeight = $(window).innerHeight()
-    $('body').height(bodyHeight);
-    $('#editor').height(bodyHeight - $('.navbar-static-top').outerHeight() - $('.navbar-fixed-bottom').outerHeight());
+$(window).resize(function() {
+  var bodyHeight = $(window).innerHeight();
+  $("body").height(bodyHeight);
+  $("#editor").height(
+    bodyHeight -
+      $(".navbar-static-top").outerHeight() -
+      $(".navbar-fixed-bottom").outerHeight()
+  );
 });
 
-
-$(window).focus(function(){
-    editor.focus();
+$(window).focus(function() {
+  editor.focus();
 });
--- src/scripts/workerService.js
+++ src/scripts/workerService.js
@@ -1,10 +1,12 @@
-var toolbox = require('sw-toolbox');
+var toolbox = require("sw-toolbox");
 toolbox.options.debug = true;
 toolbox.router.default = toolbox.fastest;
 
 // Boilerplate to ensure our service worker takes control of the page as soon
 // as possible.
-global.addEventListener('install',
-    event => event.waitUntil(global.skipWaiting()));
-global.addEventListener('activate',
-    event => event.waitUntil(global.clients.claim()));
+global.addEventListener("install", event =>
+  event.waitUntil(global.skipWaiting())
+);
+global.addEventListener("activate", event =>
+  event.waitUntil(global.clients.claim())
+);
