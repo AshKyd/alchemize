0aabf3762b95b6782ad8714673447939b88a241f
Ash Kyd <ash@kyd.com.au>
2025-08-21 22:31:54 +1000

    bug: process is undefined

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  define: {
-    "process.env": {},
+  define: { "process.env": {} },
+  build: {
+    rollupOptions: {
+      plugins: [
+        inject({
+          process: "process",
+        }),
+      ],
+    },
   },
 });
